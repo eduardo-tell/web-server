8e30ac675d11b0b040d19270e6d456672e45aac9
Eduardo Tell <eduardo-tell@hotmail.com>
2021-03-04 22:29:05 -0300

    Bundle optimization

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -15,8 +15,18 @@ plugins.push(
     })
 );
 
+plugins.push(
+    new webpack.optimize.CommonsChunkPlugin(
+        { 
+            name: 'vendor', 
+            filename: 'vendor.bundle.js'
+        }
+    )
+);
+
 if (process.env.NODE_ENV == 'production') {
 
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
     plugins.push(new babiliPlugin());
 
     plugins.push(new optimizeCSSAssetsPlugin({
@@ -30,7 +40,10 @@ if (process.env.NODE_ENV == 'production') {
 }
 
 module.exports = {
-    entry: "./app-src/app.js",
+    entry: {
+        app: "./app-src/app.js",
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
